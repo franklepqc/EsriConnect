79f5fb81ca375d59926db22984f3b39b6b456387
François Lépine <lepine.francois@gatineau.ca>
2018-03-05 21:34:12 -0500

    Automatisation de la publication sur NuGet.

---
+++ Scripts/PublierPaquets.ps1
@@ -0,0 +1,21 @@
+﻿# Variables.
+$version = (Read-Host -Prompt "Entrez le n° de version").ToString()
+$cleApi = (Read-Host -Prompt "Entrez la clé d'API").ToString()
+$source = "https://api.nuget.org/v3/index.json"
+
+$cheminProjetInterfaces = "..\ESRI.NetCore.Interfaces\ESRI.NetCore.Interfaces.csproj"
+$cheminProjetClient = "..\ESRI.NetCore\ESRI.NetCore.csproj"
+$cheminPaquetInterfaces = "..\ESRI.NetCore.Interfaces\bin\Release\ESRIConnect.Interfaces." + $version + ".nupkg"
+$cheminPaquetClient = "..\ESRI.NetCore\bin\Release\ESRIConnect." + $version + ".nupkg"
+
+# Paquet ESRIConnect.Interfaces.
+Write-Information -MessageData "Création du paquet ESRIConnect.Interfaces..."
+dotnet pack -c Release $cheminProjetInterfaces
+Write-Information -MessageData "Publication du paquet ESRIConnect.Interfaces..."
+dotnet nuget push $cheminPaquetInterfaces -k $cleApi -s $source
+
+# Paquet ESRIConnect.
+Write-Information -MessageData "Création du paquet ESRIConnect..."
+dotnet pack -c Release $cheminProjetClient
+Write-Information -MessageData "Publication du paquet ESRIConnect..."
+dotnet nuget push $cheminPaquetClient -k $cleApi -s $source
