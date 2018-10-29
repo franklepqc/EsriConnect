6014e054c47b736d3e1e0cbf98d4f34ca7b8e37e
François Lépine <francois.lepine@gmail.com>
2018-10-28 21:45:23 -0400

    Version et noms de paquets.

--- Scripts/PublierPaquets.ps1
+++ Scripts/PublierPaquets.ps1
@@ -3,10 +3,13 @@ $version = (Read-Host -Prompt "Entrez le n° de version").ToString()
 $cleApi = (Read-Host -Prompt "Entrez la clé d'API").ToString()
 $source = "https://api.nuget.org/v3/index.json"
 
+# Projets.
 $cheminProjetInterfaces = "..\ESRI.NetCore.Interfaces\ESRI.NetCore.Interfaces.csproj"
-$cheminProjetClient = "..\ESRI.NetCore\ESRI.NetCore.csproj"
-$cheminPaquetInterfaces = "..\ESRI.NetCore.Interfaces\bin\Release\ESRIConnect.Interfaces." + $version + ".nupkg"
-$cheminPaquetClient = "..\ESRI.NetCore\bin\Release\ESRIConnect." + $version + ".nupkg"
+$cheminProjetClient = "..\ESRI.NetCore\ESRIConnect.NetCore.csproj"
+
+# Paquets.
+$cheminPaquetInterfaces = "..\ESRI.NetCore.Interfaces\bin\Release\netcoreapp2.0\ESRIConnect.Interfaces." + $version + ".nupkg"
+$cheminPaquetClient = "..\ESRI.NetCore\bin\Release\netcoreapp2.0\ESRIConnect." + $version + ".nupkg"
 
 # Paquet ESRIConnect.Interfaces.
 Write-Information -MessageData "Création du paquet ESRIConnect.Interfaces..."
